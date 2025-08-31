dc45160d0e74080ce2a9e155abb0376d95268c3c
syedshafinahmed <shafinahmed.cse@gmail.com>
2025-08-31 21:15:48 +0600

    added

---
+++ data-type.js
@@ -0,0 +1,11 @@
+// primitive
+let data = 4;
+data = "humm";
+data = false;
+data = undefined;
+data = null;
+
+// non-primitive
+data = { name: "shafin", age: 23};
+data = [12, 45];
+console.log(typeof data);
