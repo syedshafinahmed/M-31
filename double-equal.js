01e2d6dd55f8b42b416ea552963ec2c353e18c34
syedshafinahmed <shafinahmed.cse@gmail.com>
2025-09-01 21:10:58 +0600

    added

--- double-equal.js
+++ double-equal.js
@@ -1,1 +1,17 @@
+console.log(2 == 2);
+console.log(2 == '2');
+// if one value is string and another value is number then it converts into number then compares
 
+// if one is boolean and another is number, converts to number
+// true ==> 1
+// false ==> 0
+console.log(true == 1);
+console.log(true == '1');
+console.log(true == 0);
+console.log(undefined == null);
+console.log([] == '');
+console.log([5] == '5');
+console.log(typeof[5].toString());
+console.log({} == {});
+console.log([] == []);
+console.log([] == {});
