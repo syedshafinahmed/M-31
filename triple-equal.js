1fb76396171654ae8bd4b10584721f82c5998d9b
syedshafinahmed <shafinahmed.cse@gmail.com>
2025-09-01 21:08:25 +0600

    added

---
+++ triple-equal.js
@@ -0,0 +1,6 @@
+// === first checks the type, if its different, it returns false
+console.log(2 === 2);
+console.log(2 === '2');
+console.log(null === undefined);
+console.log(0 === '');
+console.log([] === '');
